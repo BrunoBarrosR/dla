0e0272d97d7ff44503a9bb4bb231993b2005a321
BrunoBarrosR <123647715+BrunoBarrosR@users.noreply.github.com>
2025-04-14 10:11:47 -0300

    Dia 9 - Completo

---
+++ src/semana2/dia9/Desafio01_FuncoesArrayList.java
@@ -0,0 +1,51 @@
+package semana2.dia9;
+
+public class Desafio01_FuncoesArrayList {
+
+    static String[] array = new String[]{"VW Polo", "Fiat Strada", "GM Onix", "Fiat Argo", "Hyundai HB20", "GM Onix Plus", "VW T-Cross", "GM Tracker", "Fiat Mobi", "VW Saveiro"};
+
+    public static void main(String[] args) {
+
+        System.out.println(indexOf("GM Onix"));
+        System.out.println(includes("Hyundai HB20"));
+        System.out.println(includes("L200"));
+        System.out.println(lastIndexOf("GM Onix"));
+
+    }
+
+
+    public static int indexOf(String elemento){
+        int index = -1;
+        for(int i = 0; i < array.length; i++) {
+            if(array[i].equals(elemento)){
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+
+    public static boolean includes(String elemento) {
+        boolean resposta = false;
+        
+        for(int i = 0; i < array.length; i++) {
+            if (array[i].equals(elemento)) {
+                resposta = true;
+                break;
+            }            
+        }
+        return resposta;
+    }
+
+    public static int lastIndexOf(String elemento) {
+        int index = -1;
+
+        for (int i = array.length-1 ; i >= 0; i--) {
+            if(array[i].equals(elemento)) {
+                index = i;
+            }
+        }
+        return index;
+    }
+
+}
---
+++ src/semana2/dia9/Desafio02_Slice.java
@@ -0,0 +1,42 @@
+package semana2.dia9;
+
+import java.util.Arrays;
+
+public class Desafio02_Slice {
+
+    //O segundo desafio será recriar o Slice e fazer o seu da sua maneira. Lembre-se que se nenhum parâmetro for fornecido ele simplesmente copia o array original inteiro.
+    //
+    //Dica: Para funcionar parecido com o original, será necessário enviar como primeiro parâmetro o próprio array.
+    //O índice de início e fim podem receber valores por padrão também.
+
+    static String[] array = new String[]{"VW Polo", "Fiat Strada", "GM Onix", "Fiat Argo", "Hyundai HB20", "GM Onix Plus", "VW T-Cross", "GM Tracker", "Fiat Mobi", "VW Saveiro"};
+
+    public static void main(String[] args) {
+
+        System.out.println(Arrays.toString(slice(array, -1, 5)));
+        System.out.println(Arrays.toString(slice(array)));
+    }
+
+    static String[] slice(String[] array) {
+        return slice(array,  0, array.length);
+    }
+
+    static String[] slice(String[] array, int primeiroElemento, int ultimoElemento) {
+
+        if(primeiroElemento < 0) primeiroElemento = 0;
+        if(ultimoElemento < array.length) ultimoElemento = array.length;
+        if (primeiroElemento > ultimoElemento) primeiroElemento = ultimoElemento;
+
+        int quantidadeElementos = ultimoElemento - primeiroElemento;
+        String[] arrayResultante = new String[quantidadeElementos];
+        int contador = 0;
+
+        for (int i = primeiroElemento; i < ultimoElemento; i++) {
+
+            arrayResultante[contador] = array[i];
+
+            contador = contador + 1;
+        }
+        return arrayResultante;
+    }
+}
