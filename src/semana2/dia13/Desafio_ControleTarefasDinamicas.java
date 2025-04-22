85d83f9605f1ac6fdd9aad196afc6ff15e5ddfe0
BrunoBarrosR <123647715+BrunoBarrosR@users.noreply.github.com>
2025-04-22 19:32:35 -0300

    Dia 13 - Completo

---
+++ src/semana2/dia13/Desafio_ControleTarefasDinamicas.java
@@ -0,0 +1,100 @@
+package semana2.dia13;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Desafio_ControleTarefasDinamicas {
+
+    static Deque<String> tarefas = new LinkedList<>();
+
+    public static void main(String[] args) {
+
+        entrarNaFila("tarefa01" );
+        entrarNaFila("tarefa02" );
+        entrarNaFila("tarefa03" );
+        entrarNaFila("tarefa04" );
+
+        System.out.println(tarefas);
+
+        aumentarPrioridade("tarefa03");
+        aumentarPrioridade("tarefa04");
+        aumentarPrioridade("Tarefa");
+
+        diminuirPrioridade("tarefa01");
+        diminuirPrioridade("tarefa01");
+
+        System.out.println(tarefas);
+
+
+    }
+
+    public static void entrarNaFila(String tarefa) {
+        tarefas.add(tarefa);
+//        tarefas.push(tarefa);
+        System.out.println("Tarefa: " + tarefa + " foi adicionada!");
+    }
+
+
+    public static void aumentarPrioridade(String tarefaAlterada) {
+
+        //transformar o deque tarefas em uma listaTarefas para utilzar indexOf
+        List<String> listaTarefas = new LinkedList<>(tarefas);
+
+        //Identificar o index da String passada
+        int indexAtual = listaTarefas.indexOf(tarefaAlterada);
+
+        //Verificar se o index identificado não é o primeiro (indexAtual > 0).
+        // IFNão sendo, lógica: diminuir uma posição dentro da lista
+        //ELSEIF (indexAtual == -1) - "Está tarefa não está na fila de produção"
+        // ELSESe for não tem como aumentar a sua prioridade. Mensagem de "Este já é o item prioritário!"
+        if(indexAtual > 0) {
+            int indexPosterior = indexAtual - 1;
+            String tempIndexPosterior = listaTarefas.get(indexPosterior);
+
+            listaTarefas.set(indexPosterior, tarefaAlterada);
+            listaTarefas.set(indexAtual, tempIndexPosterior);
+
+            System.out.println("Prioridade da tarefa " + tarefaAlterada + " foi aumentada!");
+            for (String tarefa : listaTarefas) {
+                System.out.println(tarefa);
+            }
+
+            tarefas = new LinkedList<>(listaTarefas);
+
+        } else if (indexAtual == -1) {
+            System.out.println("Esta tarefa não está na fila de produção");
+        } else {
+            System.out.println("Esta já é a tarefa prioritária!");
+        }
+    }
+
+
+    public static void diminuirPrioridade(String tarefaAlterada) {
+
+        List<String> listaTarefas = new LinkedList<>(tarefas);
+
+        int indexAtual = listaTarefas.indexOf(tarefaAlterada);
+
+        if(indexAtual < listaTarefas.size() + 1) {
+
+            int indexPosterior = indexAtual + 1;
+            String tempIndexPosterior = listaTarefas.get(indexPosterior);
+
+            listaTarefas.set(indexAtual, tempIndexPosterior);
+            listaTarefas.set(indexPosterior, tarefaAlterada);
+
+            System.out.println("Prioridade da tarefa " + tarefaAlterada + " foi diminuida!");
+            for (String tarefa : listaTarefas) {
+                System.out.println(tarefa);
+            }
+
+            tarefas = new LinkedList<>(listaTarefas);
+
+        } else if (indexAtual == -1) {
+            System.out.println("Esta tarefa não está na fila de produção");
+        } else {
+            System.out.println("Esta já é a última tarefa!");
+        }
+    }
+}
